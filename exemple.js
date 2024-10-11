2cc66ad5d33001e3b75b6702ea4f3a070eeb352f
Matteo-K <kervadecmatteo@gmail.com>
2024-10-11 16:54:27 +0200

    set swiper

--- exemple.js
+++ exemple.js
@@ -1,21 +1,18 @@
-let TrandingSlider = new Swiper(".swiper", {
+var TrandingSlider = new Swiper(".tranding-slider", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   loop: true,
   slidesPerView: "auto",
   coverflowEffect: {
-    rotate: 6,
-    stretch: 80,
+    rotate: 0,
+    stretch: 0,
     depth: 100,
     modifier: 2.5,
   },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
   },
   navigation: {
     nextEl: ".swiper-button-next",
